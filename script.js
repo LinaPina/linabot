3423c4f5f23f15e96e82dcffbecd7890a3405bf8
LinaPina <elina.davidova@gmail.com>
2016-06-29 19:14:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Lina? Just say HI to get started.')
+            return bot.say('So you want to learn about Lina? Just say HI to get started and press the buttons.')
                 .then(() => 'speak');
         }
     },
